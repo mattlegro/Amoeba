3626362e8556f9c680da36b9df90c11f25bfe55a
Matt LeGro <esmbaseball@gmail.com>
2020-09-22 13:05:58 -0600

    Some Fixes to math in DSM and vec, added some debugging text to print to cmd.

--- src/util/DSM.py
+++ src/util/DSM.py
@@ -24,7 +24,9 @@ def find_DSM_path(target: Vec3, pos_steps, velocity_steps, L_steps, controls_tra
 
     turn_step = 2 / (num_guess_paths - 1)
     turn_inputs = [ -1 + turn_step * x for x in range(num_guess_paths)]
+    print(f"turn inputs = {turn_inputs}")
     delta_t = .1
+    print(f"time step = {time_step}")
 
     if time_step <= num_time_steps:
 
@@ -40,18 +42,21 @@ def find_DSM_path(target: Vec3, pos_steps, velocity_steps, L_steps, controls_tra
             # first find distance traveled and store new location for step
 
             turn_radius = turn_inputs[t] * find_turn_radius(velocity_steps[time_step - 1].length())
-            print(turn_radius)
+            print(f"turn radius = {turn_radius}")
             if turn_radius == 0:
                 theta = 0
+                d = velocity_steps[time_step - 1].length() * delta_t
             else:
                 theta = velocity_steps[time_step - 1].length() * delta_t / abs(turn_radius)
-                d = 2 * turn_radius * math.sin( theta / 2 )
-                phi = .5 * (math.pi - theta)
-                y_disp = d * math.sin(phi)
-                if turn_radius < 0:
-                    x_disp = d * math.cos(phi)
-                else:
-                    x_disp = -1 * d * math.cos(phi)
+                d = 2 * abs(turn_radius) * math.sin( theta / 2 )
+            print(f"step displacement = {d}")
+            print(f"theta = {theta}")
+            phi = .5 * (math.pi - theta)
+            y_disp = d * math.sin(phi)
+            if turn_radius < 0:
+                x_disp = d * math.cos(phi)
+            else:
+                x_disp = -1 * d * math.cos(phi)
 
             # x and y displacement above relative to direction of travel, need relative to field xyz
             # let angle psi be between car velocity and true x_hat
@@ -65,15 +70,30 @@ def find_DSM_path(target: Vec3, pos_steps, velocity_steps, L_steps, controls_tra
             else:
                 eta = 3 * math.pi / 2 - psi
 
+            print(f"eta = {eta}")
             disp_vec = Vec3(x_disp,y_disp,0)
-            true_disp = disp_vec.rotate_to(eta)
-            test_positions[t] = pos_steps[time_step - 1] + true_disp
+            print(f"disp vec = {disp_vec}")
+            field_disp = disp_vec.rotate_xy(eta)
+            print(f"field disp = {field_disp}")
+            test_positions[t] = pos_steps[time_step - 1] + field_disp
 
-            # find and store velocity vector for step
+            # find and store velocity vector for step. first, update x,y components due to acceleration.
+            # then, rotate velocity vector to correspond with new travel direction based on above turn.
 
             throttle_accel = find_throttle_accel(velocity_steps[time_step-1].length())
             accel_vec = Vec3( throttle_accel * math.cos(psi), throttle_accel * math.sin(psi), 0)
-            test_velos[t] = velocity_steps[time_step - 1] + accel_vec * delta_t
+            new_velo = velocity_steps[time_step - 1] + accel_vec * delta_t
+
+            # if new_velo is greater than achievable max throttle speed, fix
+
+            if new_velo.length() > 1410:
+                new_velo.rescale(1410)
+            
+            # rotate to correct orientation
+
+            if turn_radius < 0:
+                theta = theta * -1
+            test_velos[t] = new_velo.rotate_xy(theta)
 
             # store controls for step
 
@@ -87,13 +107,14 @@ def find_DSM_path(target: Vec3, pos_steps, velocity_steps, L_steps, controls_tra
             potential = ( .5 * 180 * 1410 ** 2 - kinetic ) + R / (r_vec.length())
             test_lagrangians[t] = kinetic - potential
             
-        print(test_velos)
+        print(f"test_velos = {test_velos}")
+        print(f"Test Ls = {test_lagrangians}")
         idx = np.argmin(test_lagrangians)
         pos_steps[time_step] = test_positions[idx]
         velocity_steps[time_step] = test_velos[idx]
         L_steps[time_step] = test_lagrangians[idx]
-        print(test_controls[idx])
-        controls_tracker[time_step] = test_controls[idx]
+        print(f"steer = {test_controls[idx].controls.steer}")
+        controls_tracker[time_step-1] = test_controls[idx]
 
         return find_DSM_path(target, pos_steps, velocity_steps, L_steps,
                              controls_tracker, num_guess_paths, num_time_steps, time_step+1)
